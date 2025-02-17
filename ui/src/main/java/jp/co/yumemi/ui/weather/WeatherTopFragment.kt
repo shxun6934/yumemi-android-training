b2cb6dc27c7c1918e6c0308101c7996ad9cc9222
shxun6934 <23094546+shxun6934@users.noreply.github.com>
2025-02-17 20:55:33 +0900

    change R path

--- ui/src/main/java/jp/co/yumemi/ui/weather/WeatherTopFragment.kt
+++ ui/src/main/java/jp/co/yumemi/ui/weather/WeatherTopFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import jp.co.yumemi.droidtraining.R
+import jp.co.yumemi.ui.R
 
 class WeatherTopFragment : Fragment() {
 
